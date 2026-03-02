ee151222f6eb25287fa22b58be1108d1d3639650
Manivass <manivassv@gmail.com>
2026-03-02 14:04:31 +0530

    feat : created NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Body from "./Body";
+
 const App = () => {
   return (
     <div>
-      <h2 className="text-2xl underline">Hello World</h2>
+      <Body />
     </div>
   );
 };
---
+++ src/NavBar.jsx
@@ -0,0 +1,124 @@
+import { PiUserCircleDuotone } from "react-icons/pi";
+const NavBar = () => {
+  return (
+    <div>
+      <div className="navbar bg-base-100 shadow-sm h-20 px-5 py-2 ">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns=""
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a>Item 1</a>
+              </li>
+              <li>
+                <a>Parent</a>
+                <ul className="p-2">
+                  <li>
+                    <a>Submenu 1</a>
+                  </li>
+                  <li>
+                    <a>Submenu 2</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a>Item 3</a>
+              </li>
+            </ul>
+          </div>
+          <a className="hover:-translate-y-0.5 hover:shadow-sm text-xl cursor-pointer">
+            <img
+              src="https://tse2.mm.bing.net/th/id/OIP.DPb2s6XedEACYmw58wFhwgHaEK?pid=Api&P=0&h=180"
+              className="w-35 my-2 "
+            />
+          </a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <details>
+                <summary>Parent</summary>
+                <ul className="p-2 bg-base-100 w-40 z-1">
+                  <li>
+                    <a>Submenu 1</a>
+                  </li>
+                  <li>
+                    <a>Submenu 2</a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end flex gap-2">
+          {/* <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-12 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  className="w-full"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-200 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div> */}
+          <button className="cursor-pointer">
+            <div className="w-auto rounded-full flex gap-2">
+              <img
+                alt="Tailwind CSS Navbar component"
+                className="w-9 h-9"
+                src="https://w7.pngwing.com/pngs/798/436/png-transparent-computer-icons-user-profile-avatar-profile-heroes-black-profile-thumbnail.png"
+              />
+              <span className="text-xl  text-gray-400 my-1">Login/SignUp</span>
+            </div>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
